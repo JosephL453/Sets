09f641ce654aab6523c4b3aefffb6506ba9ffcd1
JosephL453 <josephlijoy453@gmail.com>
2024-08-24 15:51:58 -0500

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,41 @@
+# sets = {1, 3, 2, 4, 5, 5, 5}
+# setstr = {"One", "Two", "Three", "Three"}
+# setmix = {1, -1, "Apple", 1}
+# setx = set()
+# dictionary = {}
+# # print(type(dictionary))
+# # print(type(setx))
+# sets.add(6)
+
+# fruits = {"Apple", "Banana", "Orange"}
+# vegetables = {"Brocolli", "Apple", "Carrots"}
+# fruits.update(vegetables)
+
+# # fruits.discard("Banana")
+# # fruits.remove("Carrots")
+# fruits.discard("Celery")
+# # fruits.remove("Celery")
+# print(fruits)
+# # print(len(fruits))
+# # print(min(sets))
+# # print(max(sets))
+# print(sorted(sets))
+
+# for i in fruits:
+#     print(i)
+
+seta = {1, 3, 5, 7, 8, 9}
+setb = {2, 3, 4, 6, 8, 5}
+# print(seta|setb)
+# print(seta & setb)
+# print(seta - setb)
+# print(seta ^ setb)
+# print(seta.union(setb))
+# print(seta.intersection(setb))
+# print(seta.difference(setb))
+# print(seta.symmetric_difference(setb))
+# print(seta == setb)
+seta = seta.clear()
+print(seta)
+
+
